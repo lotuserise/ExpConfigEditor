b2e0865dfb8c2994e6bc2c801a8a6eb82bd76040
Lotus Erise <mobilis.in.mobili1982@gmail.com>
2015-02-11 16:24:18 +0900

    add_item
    アイテム処理を追加

--- Expconfig.py
+++ Expconfig.py
@@ -54,8 +54,8 @@ class Ui_MainWindow(object):
 		self.tableWidget = QtGui.QTableWidget(self.tab)
 		self.tableWidget.setGeometry(QtCore.QRect(10, 10, 731, 601))
 		self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
-		self.tableWidget.setColumnCount(23)
-		self.tableWidget.setRowCount(23)
+		self.tableWidget.setColumnCount(24)
+		self.tableWidget.setRowCount(24)
 		self.tableWidget.resize(800,600)
 
 		#label setting
@@ -86,7 +86,6 @@ class Ui_MainWindow(object):
  
  		#Load Function
 		self.retranslateUi(MainWindow)
-		self.LoadiniComboBox(MainWindow)
 		self.CreateTab(MainWindow)
 		self.LoadTale(MainWindow)
 		self.tabWidget.setCurrentIndex(0)
@@ -94,7 +93,7 @@ class Ui_MainWindow(object):
 
 	def retranslateUi(self, MainWindow):
 
-		#create Window
+		#Create Window
 		MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
 
 		#Create Button
@@ -102,71 +101,6 @@ class Ui_MainWindow(object):
 		self.pushButton.setEnabled(False)
 		self.pushButton_1.setText(_translate("MainWindow", "File Read", None))
 		self.label.setText(_translate("MainWindow", "Exp.Info.", None))
-
-	def LoadiniComboBox(self, MainWindow):
-		for combo in range(0,22):
-			self.MyCombo = QtGui.QComboBox()
-			self.tableWidget.setCellWidget(combo,0,self.MyCombo)
-
-		self.MyCombo = QtGui.QComboBox()
-		config = configparser.ConfigParser()
-		config.read('fe_tc.ini')
-		config.sections()
-
-		detector1=config['SCM1']['DetectorName']
-		#Disable of Combobox
-		self.MyCombo.setEnabled(False)
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.addItem (detector1)
-		self.tableWidget.setCellWidget(0,0,self.MyCombo)
-
-		detector2=config['SCM1']['Type[Camera]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector2)
-		self.tableWidget.setCellWidget(1,0,self.MyCombo)
-
-		detector3=config['SCM1']['HostName[Camera]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector3)
-		self.tableWidget.setCellWidget(2,0,self.MyCombo)
-
-		detector4=config['SCM1']['HostName[LossLess]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector4)
-		self.tableWidget.setCellWidget(3,0,self.MyCombo)
-
-		detector4=config['SCM1']['HostName[Storage]']
-		self.MyCombo = QtGui.QComboBox()
-#		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector4)
-		self.tableWidget.setCellWidget(4,0,self.MyCombo)
-
-		detector4=config['SCM1']['Object[CamerServer]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector4)
-		self.tableWidget.setCellWidget(5,0,self.MyCombo)
-
-		detector4=config['SCM1']['Object[VME_Board]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector4)
-		self.tableWidget.setCellWidget(6,0,self.MyCombo)
-
-		detector4=config['SCM1']['Object[CameraSelector]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector4)
-		self.tableWidget.setCellWidget(7,0,self.MyCombo)
-
-		detector4=config['SCM1']['Object[Trigger]']
-		self.MyCombo = QtGui.QComboBox()
-		self.MyCombo.setEnabled(False)
-		self.MyCombo.addItem (detector4)
-		self.tableWidget.setCellWidget(8,0,self.MyCombo)
 
 	def StorageSelect(self, MainWindow):
 		pass
@@ -177,53 +111,55 @@ class Ui_MainWindow(object):
 
 	def LoadTale(self, MainWindow):
 		MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-		#Set Hedder_Label
 
-		#onelinerでまとめる場合（リスト配列化） 
+		#csvを配列化 
 		data = [ v for v in csv.reader(open("exp_2015.csv", "r")) if len(v) != 0]
 
-		#set VerticalHeadderLabel
-		setVerticalLabels = self.tableWidget.setVerticalHeaderLabels((data[1][0],data[1][1],data[1][2],data[1][3],data[1][4],
-			data[1][5],data[1][6],data[1][7],data[1][8],data[1][9],data[1][10],data[1][11],data[1][12],data[1][13],
-			data[1][14],data[1][15],data[1][16],data[1][17],data[1][18],data[1][19],data[1][20],data[1][21],data[1][22]))
 
-		#set HorizonalHeadderLabel
+		#DetectorList
+		detector_list=[]
+		for detector in range(0,len(data[2])):
+			dl=data[2][detector]
+			detector_list.append(dl)
 
+		for i in range(0,len(detector_list)-1):
+			self.MyCombo = QtGui.QComboBox()
+			self.MyCombo.addItem(detector_list[i])
+			self.MyCombo.setEnabled(False)
+			self.tableWidget.setCellWidget(i,0,self.MyCombo)
+
+		'''
+		detector_list=[]
+		for i in range(2,len(data)):
+			for j in range(0,len(data[2])):
+				dl=data[i][j]
+				detector_list.append(dl)
+
+		for i in range(0,len(detector_list)-1):
+			for j in range(0,len(detector_list[1])):
+				self.MyCombo = QtGui.QComboBox()
+				self.MyCombo.addItem(detector_list[i])
+				self.MyCombo.setEnabled(False)
+				self.tableWidget.setCellWidget(i,j,self.MyCombo)
+		'''
+
+		#VerticalHeadderLabels
+		vertical_header=[]
+		for row in range(0,len(data[1])):
+			list1=data[1][row]
+			vertical_header.append(list1)
+
+		setVerticalLabels = self.tableWidget.setVerticalHeaderLabels(vertical_header)
+		self.tableWidget.setRowCount((len(data[1])))
+
+		#HorizonalHeadderLabels
 		horizontal_header=[]
-		for a in range(2,len(data)):
-			list1=data[a][0]
-			horizontal_header.append(list1)
+		for col in range(2,len(data)):
+			list2=data[col][0]
+			horizontal_header.append(list2)
 
 		SetHorizontalLabels = self.tableWidget.setHorizontalHeaderLabels(horizontal_header)
 		self.tableWidget.setColumnCount((len(data)-2))
-
-#		exp_csv = open("exp_2015.csv",'rt')
-#		lines = exp_csv.readlines()
-#		exp_csv.close()
-#		addr = lines[1]
-#		addr1=addr.split(",")
-#		vh = list(addr1)
-#		self.tableWidget.setColumnCount(len(cals2))
-
-#		setVerticalLabels=self.tableWidget.setVerticalHeaderLabels((vh[0],vh[1],vh[2],vh[3],vh[4],vh[5],vh[6],vh[7],
-#			vh[8],vh[9],vh[10],vh[11],vh[12],vh[13],vh[14],vh[15],vh[16],vh[17],vh[18],vh[19],vh[20],vh[21],vh[22]))
-
-#		addr = lines[2]
-#		addr1=addr.split(",")
-#		hl = list(addr1)
-#		setHorizontalLabels =self.tableWidget.setHorizontalHeaderLabels((hl[0],hl[1],hl[2],))
-
-#		vertical_header = csv.reader(open('exp.csv','rt'), delimiter=',')
-#		cals1=list(vertical_header)
-#		self.tableWidget.setColumnCount(len(cals1))
-#		for data1 in cals1:
-#			setcaldata1 = self.tableWidget.setVerticalHeaderLabels(data1)
-
-#		horizontal_header = csv.reader(open('horizon.csv','rt'), delimiter=',')
-#		cals2=list(horizontal_header)
-#		self.tableWidget.setColumnCount(20)
-#		for data2 in cals2:
-#			setcaldata2 =self.tableWidget.setHorizontalHeaderLabels(data2)
 
 	def FileSave(self):
 		pass
